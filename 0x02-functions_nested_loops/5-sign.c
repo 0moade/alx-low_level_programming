6afd3d665f5ea9ec4a6835453781c430adebee49
matthew <matthewkadewole@gmail.com>
2022-09-14 07:44:42 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * @n: number input
  * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
  */
-int print_sign(int n)
+int print_sign(int n);
 {
 	if (n > 0)
 	{
@@ -19,6 +19,6 @@ int print_sign(int n)
 	else
 	}
 		_putchar ('-');
-		return (-1);
+		return ('-1');
 	}
 }
