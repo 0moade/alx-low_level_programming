56fda53fd19acf3e7d0565bd0fae23ff87330075
matthew <matthewkadewole@gmail.com>
2022-09-22 06:47:20 -0700

    r

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,23 +5,15 @@
  * @s: string
  * Return: encoded string `s`
  */
-
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet_it[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+		for (j = 0; j < 10; j++)
+			if (s[i] == letters[j])
+				s[i] = leet_it[j];
 	return (s);
 }
