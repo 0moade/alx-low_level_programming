4c6b494b7b173892846b71475feb6fe7217c11a4
matthew <matthewkadewole@gmail.com>
2022-10-20 06:36:15 -0700

    correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,14 +9,11 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current, *next;
-
-	current = head;
-	while (current != NULL)
+	if (head != NULL)
 	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
+		if (head->next != NULL)
+			free_list(head->next);
+		free(head->str);
+		free(head);
 	}
 }
